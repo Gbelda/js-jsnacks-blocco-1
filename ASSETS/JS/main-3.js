ffaab6606ce88e626031f47b4e8e14c4f9417bf1
gbelda <gbelda56@gmail.com>
2021-10-18 15:55:15 +0200

    Added prompt number cycle

--- ASSETS/JS/main-3.js
+++ ASSETS/JS/main-3.js
@@ -1,2 +1,10 @@
 //J snack 3 - Chiedere all'utente di inserire per 10 volte un numero, il programma stampa la somma di tutti i numeri inseriti
 
+let values = [];
+
+for (let i = 1; i <= 10; i++) {
+
+    values.push(parseInt(prompt('Enter a number')));
+
+}
+
