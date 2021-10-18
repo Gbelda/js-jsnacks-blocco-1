5051e2b9faf154bfa752e1f24b841df8f21aff36
gbelda <gbelda56@gmail.com>
2021-10-18 20:29:16 +0200

    Corrected snack 8 using for and parseInt

--- ASSETS/JS/main-8.js
+++ ASSETS/JS/main-8.js
@@ -1,10 +1,15 @@
 //J snack 8 - Chiedi un numero a 4 cifre all'utente e calcola la somma di tutte le cifre che compongono il numero
 
 
-const userNumber = prompt('Enter a 4 digit number')
+let userNumbers = prompt('Enter a 4 digit number')
 
-const result = parseInt(userNumber[0]) + parseInt(userNumber[1]) + parseInt(userNumber[2]) + parseInt(userNumber[3])
+// const result = parseInt(userNumber[0]) + parseInt(userNumber[1]) + parseInt(userNumber[2]) + parseInt(userNumber[3])
+var sum = 0
+for (let index = 0; index < userNumbers.length; index++) {
 
-document.getElementById("snack-8").innerHTML = `The sum of your number is: ${result}`
+    sum += parseInt(userNumbers[index])
+
+}
+document.getElementById("snack-8").innerHTML = `The sum of your number is: ${sum}`
 
 
