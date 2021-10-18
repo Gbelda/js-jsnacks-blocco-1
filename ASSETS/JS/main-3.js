afe8b5ee4a7ff0e42982f2693f9b404599dbbe90
gbelda <gbelda56@gmail.com>
2021-10-18 16:30:01 +0200

    Complete snack 3

--- ASSETS/JS/main-3.js
+++ ASSETS/JS/main-3.js
@@ -1,10 +1,17 @@
 //J snack 3 - Chiedere all'utente di inserire per 10 volte un numero, il programma stampa la somma di tutti i numeri inseriti
 
-let values = [];
 
-for (let i = 1; i <= 10; i++) {
+firstNumber = parseInt(prompt("Insert a number"))
+secondNumber = parseInt(prompt("Insert a number"))
+thirdNumber = parseInt(prompt("Insert a number"))
+fourthNumber = parseInt(prompt("Insert a number"))
+fifthNumber = parseInt(prompt("Insert a number"))
+sixthNumber = parseInt(prompt("Insert a number"))
+seventhNumber = parseInt(prompt("Insert a number"))
+eightNumber = parseInt(prompt("Insert a number"))
+ninthNumber = parseInt(prompt("Insert a number"))
+tenthNumber = parseInt(prompt("Insert a number"))
 
-    values.push(parseInt(prompt('Enter a number')));
-
-}
-
+const result = (firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber + sixthNumber + seventhNumber + eightNumber + ninthNumber + tenthNumber)
+console.log(result);
+document.getElementById("snack-3").innerHTML = `the sum of all the numbers is ${result}`
