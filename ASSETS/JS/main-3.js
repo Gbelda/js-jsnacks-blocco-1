94fec99e70f833a7c4d875f9b6a4f9aaba4af462
gbelda <gbelda56@gmail.com>
2021-10-18 20:22:54 +0200

    corrected snack 3. by using for loop

--- ASSETS/JS/main-3.js
+++ ASSETS/JS/main-3.js
@@ -1,17 +1,18 @@
-//J snack 3 - Chiedere all'utente di inserire per 10 volte un numero, il programma stampa la somma di tutti i numeri inseriti
 
 
-firstNumber = parseInt(prompt("Insert a number"))
-secondNumber = parseInt(prompt("Insert a number"))
-thirdNumber = parseInt(prompt("Insert a number"))
-fourthNumber = parseInt(prompt("Insert a number"))
-fifthNumber = parseInt(prompt("Insert a number"))
-sixthNumber = parseInt(prompt("Insert a number"))
-seventhNumber = parseInt(prompt("Insert a number"))
-eightNumber = parseInt(prompt("Insert a number"))
-ninthNumber = parseInt(prompt("Insert a number"))
-tenthNumber = parseInt(prompt("Insert a number"))
+var numbers = [];
+for (var i = 0; i < 10; i++) {
 
-const result = (firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber + sixthNumber + seventhNumber + eightNumber + ninthNumber + tenthNumber)
-console.log(result);
-document.getElementById("snack-3").innerHTML = `the sum of all the numbers is ${result}`
+    numbers.push(parseInt(prompt('Insert a number')))
+
+}
+
+console.log(numbers);
+
+var sum = 0
+for (let index = 0; index < numbers.length; index++) {
+
+    sum += numbers[index]
+
+}
+document.getElementById("snack-3").innerHTML = `the sum of all the numbers is: ${sum}`
